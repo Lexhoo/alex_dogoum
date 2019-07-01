099971e7363cf4496ea754cc7f2f3652b2ba2ba9
Alex Dogoum <lexhoo@yahoo.fr>
2019-07-01 15:36:59 +0200

    020

--- front/src/SignUp.jsx
+++ front/src/SignUp.jsx
@@ -3,19 +3,42 @@ import React, {Component} from 'react';
 class Email extends Component {
   constructor(props){
     super(props);
-    this.state = {email: ''};
+    this.state = {
+    email: 'mon@email.com',
+    password: "monPassw0rd",
+    passwordbis:"passwordbis",
+    name: "James",
+    lastname: "Bond"
+    };
 
-    this.updateEmailField = this.updateEmailField.bind(this);
+
+    
   }
-  updateEmailField(event) {
-    this.setState({email: event.target.value});
+  updateEmailField = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+  })
+}
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(`a name was submitted : ${JSON.stringify(this.state, 1, 1)}`);
   }
 
   render() {
     return(
-    <div> nouveau composant 
-      <h1>{this.state.email}</h1>
-      <input type="email" name="email" value={this.state.value} onChange={this.updateEmailField}/>
+    <div> 
+
+      <h1>{JSON.stringify(this.state,1, 1)}</h1>
+      <form className="formulaire" onSubmit={this.handleSubmit}>
+
+      <input type="email" name="email" value={this.state.email} onChange={this.updateEmailField}/>
+      <input type="password" name="password" value={this.state.password} onChange={this.updateEmailField}/>
+      <input type="passwordbis" name="passwordbis" value={this.state.passwordbis} onChange={this.updateEmailField}/>
+      <input type="string" name="name" value={this.state.name} onChange={this.updateEmailField}/>
+      <input type="lastname" name="lastname" value={this.state.lastname} onChange={this.updateEmailField}/>
+      <input type="submit" value="soumettre"/>
+      </form>
     </div>
 
     );
