a2a8349e53ad7be93dd1f420a98fea848876627e
Alex Dogoum <lexhoo@yahoo.fr>
2019-07-04 16:31:28 +0200

    050

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../../helpers/db')
+const port = process.env.PORT || 5000;
 
-router.post('/signup', function(req, res, next) {
-  const post  = req.body;
-  connection.query('INSERT INTO users SET ?', post,  (err) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send('Erreur lors de l/ajout d/un mail');
-      
-    }else {
-      res.sendStatus(200);
-    }
+router.post('/signup', function(req, res) { console.log(req.body)
   
-  });
-  });
+  connection.query('INSERT INTO users SET ?', req.body, (error) => {
+    console.log(error);
+    if (error)
+    res.status(500).json({ flash:  error.message });
+else
+    res.status(200).json({ flash:  "User has been signed up !" });
+  }
+);
+});
 
   module.exports = router;
